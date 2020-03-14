a59ebdcbe910d3d14a00cd76a7f1d479e5626b22
Laura Collins <laura.e.collins1@gmail.com>
2020-03-14 11:47:56 -0500

    watercraft

---
+++ Garage/Watercraft.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Garage
+{
+    class Watercraft
+    {
+        public int Fuel { get; set; }
+        public int Passengers { get; set; }
+        public VehicleColor Color { get; set; }
+
+        public void Refuel(int numberOfGallons)
+        {
+            if (numberOfGallons <= Fuel)
+            {
+                Console.WriteLine($"You put {numberOfGallons} gallons of fuel in the vehicle.");
+            }
+            else
+            {
+                Console.WriteLine($"{numberOfGallons} gallons of fuel is too much for your vehicle.")
+            };
+        }
+
+        public void Drive(int miles)
+        {
+            Console.WriteLine($"You drive the vehicle {miles} miles.");
+        }
+    }
+}
